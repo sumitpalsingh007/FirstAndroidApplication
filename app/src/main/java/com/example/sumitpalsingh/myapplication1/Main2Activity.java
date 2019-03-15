374d186180574ceca41ca8da48d59d5d43d85cbe
Vivek Kumar <vivek.kumar@akqa.com>
2019-03-15 12:02:39 +0530

    commit

---
+++ app/src/main/java/com/example/sumitpalsingh/myapplication1/Main2Activity.java
@@ -0,0 +1,38 @@
+package com.example.sumitpalsingh.myapplication1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private Button button = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = getIntent();
+        intent.getComponent();
+    }
+
+}
